be118387361c7ea5ade6951751501b011b1a1c67
lucas-spacecode <57467637+lucas-spacecode@users.noreply.github.com>
2020-01-25 04:18:44 -0300

    Add files via upload

---
+++ variaveisEconstantes.js
@@ -0,0 +1,12 @@
+//As variáves do tipo var podem ser redeclaradas posteriormente
+var a = 30
+
+//As variáves do tipo let n podem ser redeclaradas 
+let b = 20
+
+console.log(a, b)
+
+//const são valores constantes invariáveis
+const c = 15
+
+console.log(c)
